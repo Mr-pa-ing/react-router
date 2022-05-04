be2dc53e06bf9150c92862ae99736e5a6241e151
Mr-pa-ing <paingphyozaw54@gmail.com>
2022-05-04 11:43:15 +0630

    auth

--- src/App.js
+++ src/App.js
@@ -10,35 +10,42 @@ import OrderSummary from "./components/OrderSummary";
 import Product from "./components/Product";
 import UserDetails from "./components/UserDetails";
 import Users from "./components/Users";
+import Profile from "./components/Profile";
+import { AuthProvider } from "./components/auth/auth";
+import Login from "./components/auth/Login";
+import RequireAuth from "./components/auth/RequireAuth";
+
 const LazyAbout = React.lazy(() => import('./components/About'));
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route
-         path="about"
-         element={
-           <React.Suspense fallback='Loading...'>
-             <LazyAbout/>
-           </React.Suspense>
-         }
-     />
-      <Route path="order-summary" element={<OrderSummary/>} />
-      <Route path="*" element={<NoMatch/>} />
-      <Route path="product" element={<Product/>} >
-        <Route index element={<FeaturedProduct/>} />
-        <Route path="featured" element={<FeaturedProduct/>} />
-        <Route path="new" element={<NewProduct/>} />
-      </Route>
-      <Route path="users" element={<Users/>}>
-        <Route path=":userId" element={<UserDetails/>}/>
-        <Route path="admin" element={<Admin/>} />
-      </Route>
-    </Routes>
-    </>
+        <Route path="/" element={<Home/>} />
+        <Route
+          path="about"
+          element={
+            <React.Suspense fallback='Loading...'>
+              <LazyAbout/>
+            </React.Suspense>
+          }
+        />
+        <Route path="order-summary" element={<OrderSummary/>} />
+        <Route path="*" element={<NoMatch/>} />
+        <Route path="product" element={<Product/>} >
+          <Route index element={<FeaturedProduct/>} />
+          <Route path="featured" element={<FeaturedProduct/>} />
+          <Route path="new" element={<NewProduct/>} />
+        </Route>
+        <Route path="users" element={<Users/>}>
+          <Route path=":userId" element={<UserDetails/>}/>
+          <Route path="admin" element={<Admin/>} />
+        </Route>
+        <Route path="/profile" element={<RequireAuth><Profile/></RequireAuth>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useAuth } from './auth/auth'
 
 const Navbar = () => {
+    const auth = useAuth();
+
     const styleNavbar = ({isActive}) => {
         return {
             textDecoration: isActive ? 'underline' : 'none',
@@ -14,6 +17,10 @@ const Navbar = () => {
         <NavLink style={styleNavbar} to="/">Home</NavLink>
         <NavLink style={styleNavbar} to="/about">About</NavLink>
         <NavLink style={styleNavbar} to="/product">Product</NavLink>
+        <NavLink style={styleNavbar} to="/profile">Profile</NavLink>
+        {
+          !auth.user && <NavLink style={styleNavbar} to="/login">Login</NavLink>
+        }
     </nav>
   )
 }
